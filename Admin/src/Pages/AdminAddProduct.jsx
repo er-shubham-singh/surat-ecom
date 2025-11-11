3598d4d184d62f452da524916e34c51388244661
Paras Negi <paras22negi@gmail.com>
2025-11-11 11:16:15 +0530

    coustomer page added

--- Admin/src/Pages/AdminAddProduct.jsx
+++ Admin/src/Pages/AdminAddProduct.jsx
@@ -3,10 +3,10 @@ import AddProductForm from "../Common/AddProductForm";
 
 const AdminAddProduct = () => {
   return (
-    <>
+    <div className="min-h-screen bg-linear-to-br from-[#0f1120] to-bg-[#15172b] p-8">
       <h1 className="text-center text-4xl font-semibold mb-6">Add New Product</h1>
       <AddProductForm />
-    </>
+    </div>
   );
 };
 
