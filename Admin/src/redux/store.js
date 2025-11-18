6e5e95d5f323c04f26234678ec79a00b7dc5965c
er-shubham-singh <shubham.singh7985@gmail.com>
2025-11-18 13:42:50 +0530

    fix deployment issue

--- Admin/src/redux/store.js
+++ Admin/src/redux/store.js
@@ -4,18 +4,11 @@ import {thunk} from "redux-thunk"; // <-- default import
 import authReducer from "./Auth/Reducer";
 import customerProductReducer from "./Customers/Product/Reducer";
 import productReducer from "./Admin/Product/Reducer";
-import cartReducer from "./Customers/Cart/Reducer";
-import { orderReducer } from "./Customers/Order/Reducer";
 import adminOrderReducer from "./Admin/Orders/Reducer";
-import ReviewReducer from "./Customers/Review/Reducer";
 
 const rootReducers = combineReducers({
   auth: authReducer,
   customersProduct: customerProductReducer,
-  cart: cartReducer,
-  order: orderReducer,
-  review: ReviewReducer,
-
   // admin
   adminsProduct: productReducer,
   adminsOrder: adminOrderReducer,
