f44cb0ca5b88a45ec14bb9bb6b1a2762ed8de187
er-shubham-singh <165591109+er-shubham-singh@users.noreply.github.com>
2025-11-28 00:51:41 +0000

    fix file name

--- Admin/src/Redux/Customers/Payment/Action.js
+++ Admin/src/Redux/Customers/Payment/Action.js
@@ -1,4 +1,4 @@
-import api, { API_BASE_URL } from '../../../config/api';
+import api, { API_BASE_URL } from '../../../Config/api';
 import {
     CREATE_PAYMENT_REQUEST,
     CREATE_PAYMENT_SUCCESS,
